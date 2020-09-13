c7a1b5cfb418c107a11657d3b8f7f4f6b676d7e9
Clare Jackson <clare-jackson@lambdastudents.com>
2020-09-13 19:47:18 -0400

    arrays-callbacks.js complete.

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,5 +1,23 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
+// function CuboidMaker(length, width, height){
+// 	this.length = length;
+// 	this.width = width;
+// 	this.height = height;
+// }
+
+// CuboidMaker.prototype.volume = function(){
+// 	return this.length * this.width * this.height;
+// }
+
+// CuboidMaker.prototype.surfaceArea = function(){
+// 	let math = ((this.length * this.width) + (this.length * this.height) + (this.width 	* this.height));
+// 	return math * 2;
+// }
+
+// const cuboid = new CuboidMaker(4, 5, 5);
+
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
